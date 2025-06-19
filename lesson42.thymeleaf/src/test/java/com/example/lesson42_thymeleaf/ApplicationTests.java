60f62b1eebf3d51e6e1fce207220552e63dcfe5c
Tioma10 <xovrax97@mail.ru>
2025-06-19 13:18:21 +0300

    added homework 42

---
+++ lesson42.thymeleaf/src/test/java/com/example/lesson42_thymeleaf/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson42_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
